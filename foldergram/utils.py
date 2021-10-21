0f9cfaa32e1d07ea564c3b962efaa649b5124f7b
drjabaka <drjabaka@gmail.com>
2021-10-22 00:39:13 +0300

    fix sort logic

--- foldergram/utils.py
+++ foldergram/utils.py
@@ -1,11 +1,11 @@
 import os
 
-
 def sort_like_explorer(dir_path: str) -> tuple[list[str], list[str]]:
     dirs, files = [], []
     dir_and_file_list = os.listdir(dir_path)
-    dir_and_file_list.sort()
     dir_and_file_list.sort(key=lambda x: len(x))
+    dir_and_file_list.sort()
+
 
     # first dirs and then files
     for item in dir_and_file_list:
