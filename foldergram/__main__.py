51a0de0de47e8f205d2a304ae6ec3d71c421d724
drjabaka <drjabaka@gmail.com>
2021-10-22 21:37:21 +0300

    add start bot

--- foldergram/__main__.py
+++ foldergram/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+from . import bot
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--bot', '-b')
@@ -6,3 +7,5 @@ parser.add_argument('--proxy', '-p')
 args = parser.parse_args()
 
 print(args)
+
+bot.start(args.bot, args.proxy)
