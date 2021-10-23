69ec5fd194b79e07b55a612b4980551c6690984b
drjabaka <drjabaka@gmail.com>
2021-10-23 15:18:43 +0300

    add token argument

--- foldergram/__main__.py
+++ foldergram/__main__.py
@@ -4,6 +4,7 @@ from . import bot
 parser = argparse.ArgumentParser()
 parser.add_argument('--bot', '-b')
 parser.add_argument('--proxy', '-p')
+parser.add_argument('--token', '-t')
 args = parser.parse_args()
 
 print(args)
