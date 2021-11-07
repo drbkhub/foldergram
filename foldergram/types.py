5bc7d2d49e7332f619c2e9679b0956e89a65ee69
drjabaka <drjabaka@gmail.com>
2021-11-07 21:50:56 +0300

    move sorting function from utils.py and delete file

--- foldergram/types.py
+++ foldergram/types.py
@@ -258,4 +258,22 @@ class Bot:
                 else:
                     group.append(item)
 
-        return group  
+        return group
+
+
+
+def sort_like_explorer(dir_path: str) -> tuple[list[str], list[str]]:
+    dirs, files = [], []
+    dir_and_file_list = os.listdir(dir_path)
+    dir_and_file_list.sort(key=lambda x: len(x))
+    dir_and_file_list.sort()
+
+
+    # first dirs and then files
+    for item in dir_and_file_list:
+        if os.path.isfile(os.path.join(dir_path, item)):
+            files.append(item)
+        elif os.path.isdir(os.path.join(dir_path, item)):
+            dirs.append(item)
+
+    return dirs, files
